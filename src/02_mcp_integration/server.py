e82fb2a3418e1d4d9885a206a3449842bcd457db
anagabrielaoliveira <anagabrielaolvr@gmail.com>
2026-04-30 14:18:10 -0300

    add mcp integration exercise (02)

---
+++ src/02_mcp_integration/server.py
@@ -0,0 +1,66 @@
+# 2. FastMCP / MCP Client (segundo passo)
+
+    # 2.1 Entender o que é o protocolo MCP, é um servidor que expõe "tools" para LLMs chamarem via JSON
+
+        # --- o protocolo MCP serve como uma ponte entre o agente e a execução das tools. Ele expõe as tools, entao agora ao 
+        # inves do agente ter a tool, o mcp (mcp client) faz uma chamada via json para invocar a tool no mcp server que 
+        # executa a tool e devolve o resultado ao agente 
+
+    # 2.2 Criar seu próprio servidor MCP e listar as tools disponíveis
+
+    # 2.3 Chamar uma tool do MCP server manualmente, sem LLM ainda
+
+    # 2.4 Integrar seu mcp no agente criado
+
+import sys
+from typing import Any
+import httpx
+from mcp.server.fastmcp import FastMCP
+
+# 2.2 Criar seu próprio servidor MCP e listar as tools disponíveis
+
+# Initialize FastMCP Server
+mcp = FastMCP("test_server")
+
+@mcp.tool()
+async def simple_calculator(num1: float, num2: float, operator: str) -> str:
+    """Perform a basic arithmetic operation between two numbers.
+
+    Supported operators:
+    - '+' for addition
+    - '-' for subtraction
+    - '*' for multiplication
+    - '/' for division
+
+    Returns the result as a string.
+
+    Args: 
+        num1: The first number in the calculation
+        num2: The second number in the calculation
+        operator: The operator to use in the calculation
+    """
+
+    # Perform the calculation based on the operator
+    if operator == "+":
+        return {num1 + num2}
+    elif operator == "-":
+        return {num1 - num2}
+    elif operator == "*":
+        return {num1 * num2}
+    elif operator == "/":
+        if num2 == 0:
+            return "Cannot divide by zero."
+        return {num1 / num2}
+    
+    return "Unsupported operator."
+
+# Running the server 
+def main():
+    print("MCP Server is running...", file=sys.stderr) # envia a mensagem para stderr, ou seja, não interfere no MCP
+    mcp.run() # Inicia o servidor MCP 
+
+if __name__ == "__main__":
+    main()
+
+
+#  
